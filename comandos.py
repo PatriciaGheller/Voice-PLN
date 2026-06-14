ab77d5b5c5dd10d3d6cb7e699c1efeac04aad85f
PatriciaGheller <patricia.gheller.1985@gmail.com>
2026-06-14 09:27:32 -0300

    Criação do módulo comandos.py para organizar funções de execução

---
+++ comandos.py
@@ -0,0 +1,21 @@
+import os
+
+def abrir_navegador():
+    print("Abrindo navegador Chrome...")
+    os.system("start chrome.exe")
+
+def abrir_excel():
+    print("Abrindo Excel...")
+    os.system("start excel.exe")
+
+def abrir_powerpoint():
+    print("Abrindo PowerPoint...")
+    os.system("start powerpnt.exe")
+
+def abrir_edge():
+    print("Abrindo Microsoft Edge...")
+    os.system("start msedge.exe")
+
+def fechar_programa():
+    print("Encerrando Voice-PLN...")
+    return True
