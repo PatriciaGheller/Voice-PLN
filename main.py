d383aa4ff766a5ce065641917d7a95814d3fcd45
PatriciaGheller <patricia.gheller.1985@gmail.com>
2026-06-14 09:51:11 -0300

    Atualização do main.py para incluir mensagem de boas-vindas ao iniciar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from reconhecimento import ouvir_microfone
+from feedback import mensagem_boas_vindas
 
 def main():
+    mensagem_boas_vindas()
     print("=== Voice-PLN iniciado ===")
     print("Fale um comando...")
 
